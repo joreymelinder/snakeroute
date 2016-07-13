5315c9a98eff131de714caebc291aedbbda75195
mechagandhi <jorey.melinder@g.austincc.edu>
2016-07-13 16:37:56 -0500

    csv

--- snakeroute/settings.py
+++ snakeroute/settings.py
@@ -38,15 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'csvimport.app.CSVImportConf',
-    #'corsheaders',
     'map',
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
